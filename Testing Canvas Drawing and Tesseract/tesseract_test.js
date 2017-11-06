bdd60223d5aa3ddfe1bfe4850d506e3e0638f80b
eduard-tuduri <eduardgabriel96@gmail.com>
2017-11-06 22:36:18 +0200

    Simulating asynchronous questions loading locally

--- Testing Canvas Drawing and Tesseract/tesseract_test.js
+++ Testing Canvas Drawing and Tesseract/tesseract_test.js
@@ -1,13 +1,11 @@
 function testImage(){
     ctx = document.getElementById('myCanvas').getContext('2d');
     
-    console.log(ctx)
-    
     Tesseract.recognize(ctx, {
         lang: 'jpn'
     })
    .then(function(result){
-        console.log(result.text)
+        $('#result').show().html('<h1>' + result.text + '</h1>')
     })
     .catch(function(err){
         console.error(err)
