fb376895a680978e27d4691d5a1b5f61b22a89a9
Mihaila Valerio <mihaila.andrei95@gmail.com>
2017-11-12 19:56:42 +0200

    am uitat de asta

--- user static page/assets/js/tab-action.js
+++ user static page/assets/js/tab-action.js
@@ -1,12 +1,3 @@
-//$(document).ready(function() {
-//$(".btn-pref .btn").click(function () {
-//    $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
-//    // $(".tab").addClass("active"); // instead of this do the below 
-//    $(this).removeClass("btn-default").addClass("btn-primary");   
-//});
-//});
-//
-
 $(document).ready(function (e){
     
     
