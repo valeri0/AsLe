9b75ffaba176f24b84eef4fa8a034f1fed4ea2ee
Tudosa Ovidiu <tudosa.ovidius@gmail.com>
2017-11-27 18:02:54 +0200

    admin-upload-updated

--- assets/js/admin-upload.js
+++ assets/js/admin-upload.js
@@ -13,11 +13,26 @@ document.getElementById('drop-zone').addEventListener("drop", function(event) {
     event.preventDefault();
     document.getElementById('drop-zone').classList.remove('file-drag-over');
     var files = event.target.files || (event.dataTransfer ? event.dataTransfer.files : event.originalEvent.dataTransfer.files);
-    console.log(files);
     for (file of files){
+        var div = document.createElement("div");
         var para = document.createElement("p");
-        var node = document.createTextNode(file.name);
-        para.appendChild(node);
-        document.getElementById('panel-body').appendChild(para);
+        var file_icon = document.createElement("i");
+        var p_content = document.createTextNode(file.name);
+        var x_icon = document.createElement('i');
+        file_icon.setAttribute('class', 'fa fa-file-code-o');
+        file_icon.setAttribute('aria-hiden', 'true');
+        x_icon.setAttribute('class', 'fa fa-times');
+        x_icon.setAttribute('aria-hiden', 'true');
+        x_icon.setAttribute('onclick', 'remove_file(event)');
+        para.appendChild(p_content);
+        div.appendChild(file_icon);
+        div.appendChild(para);
+        div.appendChild(x_icon);
+        div.classList.add('uploaded-files');
+        document.getElementById('panel-body').appendChild(div);
     }
 });
+
+function remove_file(event) {
+    event.srcElement.parentNode.parentNode.removeChild(event.srcElement.parentNode);
+}
