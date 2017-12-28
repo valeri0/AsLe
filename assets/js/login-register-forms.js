36a81ef8bb38c64cdf0ffcfae02092c363a05f6d
Mihaila Valerio <mihaila.andrei95@gmail.com>
2017-12-28 16:43:47 +0200

    Register with firebase (part1)
    Primul draft al inregistrarii. Daca completezi bine formularul si dai "Register", pagina nu se schimba cu nimic, da in firebase apar utilizatorii noi.

--- assets/js/login-register-forms.js
+++ assets/js/login-register-forms.js
@@ -1,54 +1,152 @@
+/* PENTRU SCHIMBAREA FORMULARULUI REGISTER/LOGIN */
+
 var login_form = document.getElementById("login_form");
 var register_form = document.getElementById("register_form");
 
-    function switchToLogin() {
+function switchToLogin() {
 
-        register_form.classList.add('hidden');
-        changeBetweenForms(register_form,login_form,fadeIn);
-        login_form.classList.remove('hidden');
-    }
+    register_form.classList.add('hidden');
+    changeBetweenForms(register_form, login_form, fadeIn);
+    login_form.classList.remove('hidden');
+}
 
-    function switchToRegister(){
-        login_form.classList.add('hidden');
-        changeBetweenForms(login_form,register_form,fadeIn);
-        register_form.classList.remove('hidden');
+function switchToRegister() {
+    login_form.classList.add('hidden');
+    changeBetweenForms(login_form, register_form, fadeIn);
+    register_form.classList.remove('hidden');
 
 
-    }
+}
 
-function changeBetweenForms(form_to_disappear,form_to_appear,callback){
-    
+function changeBetweenForms(form_to_disappear, form_to_appear, callback) {
+
     fadeOut(form_to_disappear);
     callback(form_to_appear);
 
-    
+
 }
 
 
-// fade out
+function fadeOut(el) {
+    el.style.opacity = 1;
 
-
-function fadeOut(el){
-  el.style.opacity = 1;
-
-  (function fade() {
-    if ((el.style.opacity -= .1) < 0) {
-      el.style.display = "none";
-    } else {
-      requestAnimationFrame(fade);
-    }
-  })();
+    (function fade() {
+        if ((el.style.opacity -= .1) < 0) {
+            el.style.display = "none";
+        } else {
+            requestAnimationFrame(fade);
+        }
+    })();
 };
 
-function fadeIn(el, display){
-  el.style.opacity = 0;
-  el.style.display = display || "block";
+function fadeIn(el, display) {
+    el.style.opacity = 0;
+    el.style.display = display || "block";
 
-  (function fade() {
-    var val = parseFloat(el.style.opacity);
-    if (!((val += .1) > 1)) {
-      el.style.opacity = val;
-      requestAnimationFrame(fade);
-    }
-  })();
+    (function fade() {
+        var val = parseFloat(el.style.opacity);
+        if (!((val += .1) > 1)) {
+            el.style.opacity = val;
+            requestAnimationFrame(fade);
+        }
+    })();
 };
+
+
+/* --------------------------------------------------------- */
+
+
+
+
+
+
+
+
+//firebase.auth().onAuthStateChanged(onAuthStateChange);
+
+
+
+
+
+
+
+/* REGISTER */
+
+var rf = document.getElementById('rf');
+
+rf.addEventListener('submit', function (e) {
+
+    e.preventDefault();
+
+    var first_name = document.getElementById("r_first_name").value;
+    var last_name = document.getElementById("r_last_name").value;
+    var country = document.getElementById('r_country').value;
+    var email = document.getElementById('r_email').value;
+    var password = document.getElementById('r_password').value;
+    var confirm_password = document.getElementById('r_password_confirm').value;
+    
+    createUser(first_name,last_name,country,email,password);
+    
+});
+
+var inCreatingUser = false;
+
+var userData = {};
+
+function createUser(first_name, last_name, country, email, password) {
+
+    inCreatingUser = true;
+
+    userData = {
+
+        first_name: first_name,
+        
+        last_name: last_name,
+        
+        country: country,
+
+        email: email,
+        
+        role: "user"
+
+    };
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+
+        // Handle Errors here.
+
+        var errorCode = error.code;
+
+        var errorMessage = error.message;
+
+        alert(errorMessage)
+
+    });
+
+}
+
+/* --------------------------------------------------------- */
+
+
+
+
+
+
+
+
+/* LOGIN */
+
+var lf = document.getElementById('lf');
+
+lf.addEventListener('submit', function (e) {
+
+
+
+});
+
+function login() {
+    alert('login');
+}
+
+
+/* --------------------------------------------------------- */
