a702ad7aceb56cce8864d2ec7aa0dc4e95052c1d
eduard-tuduri <eduardgabriel96@gmail.com>
2017-12-28 21:39:46 +0200

    Loading data from firebase on Lesson page

--- assets/js/letter-drawing.js
+++ assets/js/letter-drawing.js
@@ -1,37 +1,80 @@
-var letter1 = 'assets/js/no.json';
-var letter2 = 'assets/js/so.json';
 var animation;
-var letters = [letter1, letter2];
-var correct_answers = [0x306E, 0x305D];
+var paths = [];
+var json = [];
+var correct_answers = [];
+var pronunciation = [];
 
 var index = 0;
 
-window.onload = InitAnimation(letter1);
+window.onload = Init();
 
 
 
-function InitAnimation(animation_json){
-    animation = bodymovin.loadAnimation({
-    container: document.getElementById('bm'),
-    renderer: 'svg',
-    loop: false,
-    autoplay: true,
-    rendererSettings: {
-        progresiveLoad: true,
-        scaleMode: 'noScale'
-    },
-    path: animation_json
+function Init() {
+    var lesson = 'Lesson 3';
+
+    firebase.database().ref('/Lessons/' + lesson).once('value').then(function (snapshot) {
+        for (let i = 0; i < snapshot.val().letters.length; i++) {
+            pronunciation.push(snapshot.val().letters[i]);
+            correct_answers.push(parseInt(snapshot.val().hex[i], 16));
+            paths.push(snapshot.val().paths[i]);
+        }
+
+        var storageRef = firebase.storage().ref();
+
+        var count = 0;
+        for (let path of paths) {
+            storageRef.child(path).getDownloadURL().then(function (url) {
+                var xhr = new XMLHttpRequest();
+                xhr.responseType = 'json';
+                xhr.open('GET', url);
+                xhr.onload = function (event) {
+                    count++;
+                    json.push(xhr.response);
+
+                    if (count === paths.length){
+                        setPronunciation(pronunciation[0]);
+                        InitAnimation(json[0]);
+                    }
+                };
+                xhr.send();
+            }).catch(function (error) {
+                console.error(error);
+            });
+        }
+
+    }).catch(function (error) {
+        console.error(error);
     });
 }
 
-function refresh(){
+function InitAnimation(animation_json) {
+    animation = bodymovin.loadAnimation({
+        container: document.getElementById('bm'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: true,
+        rendererSettings: {
+            progresiveLoad: true,
+            scaleMode: 'noScale'
+        },
+        animationData: animation_json
+    });
+}
+
+function setPronunciation(letter) {
+    document.getElementById('pron').innerText = letter;
+}
+
+function refresh() {
     document.getElementById('bm').innerHTML = '';
-    InitAnimation(letters[index]);
+    setPronunciation(pronunciation[index]);
+    InitAnimation(json[index]);
     clearArea();
 }
 
 function MoveNextQuestion() {
-    if (index < letters.length - 1) {
+    if (index < json.length - 1) {
         forgetCanvasStates();
         index++;
         refresh();
@@ -46,26 +89,26 @@ function MovePreviousQuestion() {
     }
 }
 
-function testImage(){
+function testImage() {
     ctx = document.getElementById('myCanvas').getContext('2d');
     document.body.classList.add('loading');
 
     Tesseract.recognize(ctx, {
         lang: 'jpn'
     })
-   .then(function(result){
-       console.log(result);
-       document.body.classList.remove('loading');
-        if (result.text.charCodeAt(0) === correct_answers[index]) {
-            document.getElementById('result').innerHTML = '<h1>' + result.text + ' Correct</h1>' + svg_success;
-            openModal();
-        }else{
-            document.getElementById('result').innerHTML = '<h1>' + result.text + ' Wrong</h1>' + svg_fail;
-            openModal();
-        }
-    })
-    .catch(function(err){
-        document.body.classList.remove('loading');
-        console.error(err)
-    })
+        .then(function (result) {
+            console.log(result);
+            document.body.classList.remove('loading');
+            if (result.text.charCodeAt(0) === correct_answers[index]) {
+                document.getElementById('result').innerHTML = '<h1>' + result.text + ' Correct</h1>' + svg_success;
+                openModal();
+            } else {
+                document.getElementById('result').innerHTML = '<h1>' + result.text + ' Wrong</h1>' + svg_fail;
+                openModal();
+            }
+        })
+        .catch(function (err) {
+            document.body.classList.remove('loading');
+            console.error(err)
+        })
 }
