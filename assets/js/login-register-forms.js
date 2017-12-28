d19b7a47ba41de8a9d42034b69f6e7e18305a574
Mihaila Valerio <mihaila.andrei95@gmail.com>
2017-12-28 12:44:51 +0200

    Removed JQuery and Bootstrap from startpage

--- assets/js/login-register-forms.js
+++ assets/js/login-register-forms.js
@@ -1,39 +1,54 @@
-$(document).ready(function(){
-   
-    $('#register_here_from_menu').click(function(){
-       
-        register_function();
- 
-    });
-    
-    $('#register_here').click(function(){
-        register_function();
-        
-    });
-    
-    $('#login_here').click(function(){
-       login_function(); 
-    });
-    
-    $('#login_here_from_menu').click(function(){
-       login_function(); 
-    });
-    
-    
-    function register_function(){
-        $('#login_form').fadeOut('fast',function(){
-                $('#register_form').css('visibility','visible').hide().fadeIn();
+var login_form = document.getElementById("login_form");
+var register_form = document.getElementById("register_form");
 
-           }); 
-        
+    function switchToLogin() {
+
+        register_form.classList.add('hidden');
+        changeBetweenForms(register_form,login_form,fadeIn);
+        login_form.classList.remove('hidden');
+    }
+
+    function switchToRegister(){
+        login_form.classList.add('hidden');
+        changeBetweenForms(login_form,register_form,fadeIn);
+        register_form.classList.remove('hidden');
+
 
     }
+
+function changeBetweenForms(form_to_disappear,form_to_appear,callback){
     
-    function login_function(){
-        $('#register_form').fadeOut('fast',function(){
-                $('#login_form').css('visibility','visible').hide().fadeIn().removeClass('hidden');
+    fadeOut(form_to_disappear);
+    callback(form_to_appear);
 
-           }); 
+    
+}
+
+
+// fade out
+
+
+function fadeOut(el){
+  el.style.opacity = 1;
+
+  (function fade() {
+    if ((el.style.opacity -= .1) < 0) {
+      el.style.display = "none";
+    } else {
+      requestAnimationFrame(fade);
     }
-});
+  })();
+};
 
+function fadeIn(el, display){
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += .1) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+};
