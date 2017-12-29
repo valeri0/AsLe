f4f5bdd0a74abc8e86de1c1f0e4a6d0220a8772f
Mihaila Valerio <mihaila.andrei95@gmail.com>
2017-12-29 15:30:24 +0200

    Login with google + tried to register/login with fb
    Momentan nu merge prin facebook, deoarece trebuie sa gazduim aplicatia si sa lasam crawlerii de la facebook sa vada ca suntem safe :)

--- assets/js/google-facebook-auth.js
+++ assets/js/google-facebook-auth.js
@@ -1,7 +1,7 @@
 
 var userData = {};
 
-function google_register(){
+function google_auth(){
 
     var google_provider = new firebase.auth.GoogleAuthProvider();
 
@@ -75,7 +75,79 @@ function google_register(){
     });
 }
 
+function facebook_auth(){
 
+    var facebook_provider = new firebase.auth.FacebookAuthProvider();
+
+    firebase.auth().signInWithPopup(facebook_provider).then(function(result){
+
+
+
+        var token = result.credential.accesToken;
+
+        var user = result.user;
+
+
+        var email = user.email;
+        var first_name = user.displayName.split(" ")[0];
+        var last_name = user.displayName.split(" ")[1];
+        var uid = user.uid;
+        var photo_url = user.photoURL;
+
+        console.log(user);
+
+        userData = {
+
+            first_name: first_name,
+
+            last_name: last_name,
+
+            country: "Not Defined",
+
+            photo_url: photo_url,
+
+            email: email,
+
+            role: "user",
+
+            stats: {
+
+                level:{
+
+                    color: 'grey'
+                },
+
+                per_day:{
+                    day : 1,
+                    number_of_letters_drawn: 0
+                },
+
+                per_week:{
+                    week: 1,
+                    number_of_tries: 0,
+                    number_of_successes: 0
+                }
+
+
+            }
+
+        };
+
+        saveUserData(uid,userData);
+
+    }).catch(function(error) {
+
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+
+        console.log(errorMessage);
+    });
+}
 
 function saveUserData(userId,userData){
     firebase.database().ref('users/'+userId).set(userData)
