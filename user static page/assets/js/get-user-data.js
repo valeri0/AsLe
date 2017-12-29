ecdba41104f24b19dac1c98d4e191ab7895e4500
Mihaila Valerio <mihaila.andrei95@gmail.com>
2017-12-29 14:14:29 +0200

    Template the user profile page (part 1)
    - Momentan am facut doar pentru tab-ul about (text + culoarea nivelului)
    
    - Pentru grafice astept sa mai vorbim intre noi ca sa vedem structura lor

--- user static page/assets/js/get-user-data.js
+++ user static page/assets/js/get-user-data.js
@@ -1,21 +1,33 @@
+// FISIER FACUT PENTRU GESTIONAREA DATELOR DIN BAZA DE DATE FIREBASE IMPREUNA CU PAGINA DE PREZENTARE HTML
+
+
+
+// handler pentru butonul de logout
 function logout(){
     firebase.auth().signOut().then(function(){
 
         window.location='../Start.hmtl';
 
     }).catch(function(error){
-        alert(error.message);
+        console.log(error.message);
     });
 }
+
+
 
 firebase.auth().onAuthStateChanged(onAuthStateChange);
 
 
+//functie ce verifica starea utilizatorului
 function onAuthStateChange(user) {
     if (user) {
-        //User is signed in.
+
+        // in caz ca este logat
+
         var email = user.email;
         var userId = user.uid;
+
+        // vom face apel la baza de date pentru a prelua datele despre acesta
 
         firebase.database().ref('/users/' + userId).once('value').then(function (snapshot) {
 
@@ -25,12 +37,35 @@ function onAuthStateChange(user) {
     }
     else
         {
+            // in cazul in care iese din aplicatie, va fi redirectionat la pagina de start
+
             window.location = '../Start.html';
         }
 }
 
 
+// functie ce randeaza informatiile din baza de date a utilizatorului curent in pagina HTML (folosind Mustache.js)
+
 function renderDbValuesToHtml(jsonObj){
 
     console.log(jsonObj);
+
+    // pentru tab-ul "Your details"
+
+    var output = Mustache.render("<h5>First name: <span> {{first_name}}</span></h5>\n" +
+        "                                                    <h5>Last name: <span> {{last_name}}</span></h5>\n" +
+        "                                                    <h5>Country: <span> {{country}}</span></h5>\n" +
+        "                                                    <h5>Registered at: <span> 1st Novemeber 2017</span></h5>\n"
+
+        , jsonObj );
+
+    document.getElementById('user-info').innerHTML = output;
+
+
+    // Pentru numele de sub poza de profil din header
+    document.getElementById('header-name').innerHTML = Mustache.render("{{first_name}} {{last_name}}",jsonObj);
+
+
+    // pentru culoarea nivelului (kimono + titlu) vom apela functia din level-of-belt-action.js
+    setLevelColorForKimonoAndTitle(jsonObj.stats.level.color);
 }
--- user static page/assets/js/level-of-belt-action.js
+++ user static page/assets/js/level-of-belt-action.js
@@ -1,29 +1,47 @@
-$(document).ready(function () {
+// dictionar folosit pentru culori:
 
-    // path50 path54 path58 path62
+/*
+        0%{fill:grey}
+        11%{fill:rgb(218, 165, 32)}
+        22%{fill:#d8951a}
+        33%{fill:green}
+        44%{fill: #000099}
+        55%{fill:purple}
+        66%{fill:#b30000}
+        77%{fill:brown}
+        88%{fill: black}
+        100%{fill: grey}
+ */
 
-    // title_belt_color
+var defined_colors ={
+
+    grey: 'grey',
+    yellow: 'rgb(218, 165, 32)',
+    orange: '#d8951a',
+    green: 'green',
+    blue: '#000099',
+    red: '#b30000',
+    purple: 'purple',
+    brown: 'brown',
+    black: 'black'
+
+};
 
 
-    $('#test_button_belt_level').click(function () {
+function setLevelColorForKimonoAndTitle(color){
+
+    // pentru titlu
+
+    var title = document.getElementById('title_belt_color');
+    title.style.color = defined_colors[color];
+    title.textContent = color;
+
+    // pentru kimono
+
+    document.getElementById('path50').style.fill = defined_colors[color];
+    document.getElementById('path54').style.fill = defined_colors[color];
+    document.getElementById('path58').style.fill = defined_colors[color];
+    document.getElementById('path62').style.fill = defined_colors[color];
 
 
-        if ("rgb(0, 0, 153)" == $('#title_belt_color').css('color')) {
-
-            $('#title_belt_color').css('color', 'rgb(218, 165, 32)');
-            $('#title_belt_color').text('yellow');
-
-            $('#path50').css("fill", "rgb(218, 165, 32)");
-            $('#path54').css("fill", "rgb(218, 165, 32)");
-            $('#path58').css("fill", "rgb(218, 165, 32)");
-            $('#path62').css("fill", "rgb(218, 165, 32)");
-
-
-        }
-
-    });
-    
-    
-    $('[data-toggle="tooltip"]').tooltip();
-
-});
+}
